30de2a3d4e955fb9777b16602a7e0088024b263a
S.J. Park <shackleton@gm.gist.ac.kr>
2022-03-03 22:36:18 +0900

    Update TravelController.java
    Change to use a ModelAttribute annotation. (just use singular function and variable)

--- back-end/src/main/java/com/spring/mydiv/Controller/TravelController.java
+++ back-end/src/main/java/com/spring/mydiv/Controller/TravelController.java
@@ -3,7 +3,11 @@ package com.spring.mydiv.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.mydiv.Dto.Person;
 import com.spring.mydiv.Dto.Travel;
 import com.spring.mydiv.Service.TravelService;
 
@@ -12,11 +16,15 @@ public class TravelController {
 	@Autowired
 	private TravelService travelService;
 	
-	public String Create(Model model) {
-		Travel travel = new Travel("여수 여행");
+	@RequestMapping("/")
+	public ModelAndView personCreate(@ModelAttribute("travel") Travel travel) {
+		
 		travelService.CreateTravel(travel);
 		
-		return "Controller --> Service : Success";
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/"); // 메인으로 돌아가기
+		mv.addObject("main", "메인 화면를 나타내는데 필요한 모든 데이터"); 
+		return mv;
 		
 	}
 }
